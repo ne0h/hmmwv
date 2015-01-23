7e63233984feee48b63ecb8c78fb507f8efb7fcd
Marcel Brüggebors <marcel@nightsoul.org>
2015-01-23 15:27:01 +0100

    Remote fixes, speed-button/speed scaling

--- ros_workspace/src/hmmwv/src/remotecontrol.cpp
+++ ros_workspace/src/hmmwv/src/remotecontrol.cpp
@@ -1,4 +1,5 @@
 #include "joystick.hpp"
+// #include "Rotate.h"
 
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
@@ -7,7 +8,7 @@
 #include <math.h>
 
 const int DEAD_ZONE = 16384;
-const int AXIS_MAX = 32767;
+const int AXIS_MAX = 32767.0;
 
 using namespace ros;
 using namespace std;
@@ -20,25 +21,24 @@ int main(int argc, char **argv) {
 	cout << "Used controller: " << name.c_str() << endl;
 	
 	// init ros
-	double linear, angular, l_scale, a_scale;
+	double l_scale = 1.0;
+	double a_scale = 1.0;
 
 	init(argc, argv, "remotecontrol");
 	NodeHandle n;
 	Publisher pub;
 	
-	linear = angular = 0.0;
-	l_scale = a_scale = 1.0;
 	n.param("scale_angular", a_scale, a_scale);
   	n.param("scale_linear", l_scale, l_scale);
 
 	pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
+	// Create a fresh publisher before using this line...
+	// pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_rotate", 1);
 
 	// startup key loop
 	// use button.0 to stop
 	bool quit  = false;
 	while (!quit) {
-		linear = angular = 0.0;
-		
 		// get axis values
 		// use axis.0 for left/right
 		// and axis.1 for up/down
@@ -51,17 +51,26 @@ int main(int argc, char **argv) {
 			quit = true;
 		
 		// calculate position values
-		angular = (-1.0) * axis.at(0) / AXIS_MAX;	
-		linear  = (-1.0) * axis.at(1) / AXIS_MAX;
+		double angular = (-1.0) * axis.at(0) / AXIS_MAX;
+		double linear  = (-1.0) * axis.at(1) / AXIS_MAX;
+		double stick2y = (-1.0) * axis.at(2) / AXIS_MAX;
 
-		angular = min(angular, 1.0);
-		angular = max(angular, -1.0);
-		linear = min(linear, 1.0);
-		linear = max(linear, -1.0);
+		angular = min(max(angular, -1.0), 1.0);
+		linear = min(max(linear, -1.0), 1.0);
+		stick2y = min(max(stick2y, -1.0), 1.0);
+		
+		// SPEEED-BUTTON!
+		if(!buttons.at(1)) {
+			angular *= 0.25;
+			linear *= 0.25;
+		}
 
 		geometry_msgs::Twist twist;
 		twist.angular.z = angular;
-		twist.linear.x  = linear;
+		twist.linear.x = linear;
+
+		// Rotate rotate;
+		// rotate.linear.x = stick2y;
 		
       	pub.publish(twist);
 	}
