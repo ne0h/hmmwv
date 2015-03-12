24db7bacb856c0c06f975ca973eb0a28723924b2
Marcel Brüggebors <lists@nightsoul.org>
2015-03-12 18:23:03 +0100

    Use ROS timers, actually initialize SDL

--- ros_workspace/src/hmmwv/src/remotecontrol.cpp
+++ ros_workspace/src/hmmwv/src/remotecontrol.cpp
@@ -13,73 +13,66 @@ const int AXIS_MAX = 32767;
 using namespace ros;
 using namespace std;
 
+Joystick joystick;
+Publisher pub;
+
+void updateRemote(const TimerEvent&) {
+	// get axis values
+	// use axis.0 for left/right
+	// and axis.1 for up/down
+	JoystickEvent event = joystick.getEvent();
+	vector<short> axis = event.getAxis();
+
+	// check for button.0 to stop
+	vector<bool> buttons = event.getButtons();
+
+	// calculate position values
+	double angular = (-1.0) * axis.at(0) / AXIS_MAX;
+	double linear  = (-1.0) * axis.at(1) / AXIS_MAX;
+	double stick2y = (-1.0) * axis.at(2) / AXIS_MAX;
+
+	angular = min(max(angular, -1.0), 1.0);
+	linear = min(max(linear, -1.0), 1.0);
+	stick2y = min(max(stick2y, -1.0), 1.0);
+
+	// SPEEED-BUTTON!
+	if (!buttons.at(1)) {
+		angular *= 0.25;
+		linear *= 0.25;
+	}
+	if (!buttons.at(2)) {
+		stick2y *= 0.1; // Scale wheel rotation nice and slow
+	}
+
+	if (linear < 0) {
+		angular *= (-1.0);
+	}
+
+	geometry_msgs::Twist twist;
+	twist.angular.z = angular;
+	twist.angular.y = stick2y;
+	twist.linear.x = linear;
+
+	// Rotate rotate;
+	// rotate.linear.x = stick2y;
+	pub.publish(twist);
+}
+
 int main(int argc, char **argv) {
 	// init sdl and connect to controller
-	Joystick joystick;
+	SDL_Init(SDL_INIT_JOYSTICK);
 	string name = joystick.getName();
 	cout << "Used controller: " << name.c_str() << endl;
 	
 	// init ros
-	double l_scale = 1.0;
-	double a_scale = 1.0;
-
 	init(argc, argv, "remotecontrol");
 	NodeHandle n;
-	Publisher pub;
-	
-	n.param("scale_angular", a_scale, a_scale);
-  	n.param("scale_linear", l_scale, l_scale);
-
 	pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
 	// Create a fresh publisher before using this line...
 	// pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_rotate", 1);
 
-	// startup key loop
-	Rate rate(60.0/*Hz*/);
-	while (n.ok()) {
-		spinOnce();
-
-		// get axis values
-		// use axis.0 for left/right
-		// and axis.1 for up/down
-		JoystickEvent event = joystick.getEvent();
-		vector<short> axis = event.getAxis();
-		
-		// check for button.0 to stop
-		vector<bool> buttons = event.getButtons();
-
-		// calculate position values
-		double angular = (-1.0) * axis.at(0) / AXIS_MAX;
-		double linear  = (-1.0) * axis.at(1) / AXIS_MAX;
-		double stick2y = (-1.0) * axis.at(2) / AXIS_MAX;
-
-		angular = min(max(angular, -1.0), 1.0);
-		linear = min(max(linear, -1.0), 1.0);
-		stick2y = min(max(stick2y, -1.0), 1.0);
-		
-		// SPEEED-BUTTON!
-		if (!buttons.at(1)) {
-			angular *= 0.25;
-			linear *= 0.25;
-		}
-		if (!buttons.at(2)) {
-			stick2y *= 0.1; // Scale wheel rotation nice and slow
-		}
-
-		if (linear < 0) {
-			angular *= (-1.0);
-		}
-
-		geometry_msgs::Twist twist;
-		twist.angular.z = angular;
-		twist.angular.y = stick2y;
-		twist.linear.x = linear;
-
-		// Rotate rotate;
-		// rotate.linear.x = stick2y;
-		pub.publish(twist);
-		rate.sleep();
-	}
-
+	// startup main loop
+	Timer remoteTimer = n.createTimer(Duration(0.166 /*60 Hz*/), updateRemote);
+	spin();
 	return 0;
 }
