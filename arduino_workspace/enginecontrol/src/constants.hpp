6a66fa33dbe2304173dfa5f7bd90da6d0691477e
Max <mail@ne0h.de>
2015-04-16 16:16:58 +0200

    enginecontrol: drive monitoring pins correct

--- arduino_workspace/enginecontrol/src/constants.hpp
+++ arduino_workspace/enginecontrol/src/constants.hpp
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+// pin settings
 const uint32_t	BAUDRATE		=	115200;
 const uint8_t	BUFFER_LENGTH	=	16;
 const uint8_t	CMD_LENGTH		=	4;
@@ -10,19 +11,22 @@ const uint8_t	CMD_LENGTH		=	4;
 const uint8_t	DRIVE_LEFT_EN	=	35;
 const uint8_t	DRIVE_LEFT_DIR	=	34;
 const uint8_t	DRIVE_LEFT_SPD	=	3;
-const uint8_t	DRIVE_LEFT_MNT	=	5;		// pin 18
+const uint8_t	DRIVE_LEFT_MNT	=	2;		// pin 19
 
 const uint8_t	DRIVE_RIGHT_EN	=	37;
 const uint8_t	DRIVE_RIGHT_DIR =	36;
 const uint8_t	DRIVE_RIGHT_SPD	=	2;
-const uint8_t	DRIVE_RIGHT_MNT	=	4;		// pin 19
+const uint8_t	DRIVE_RIGHT_MNT	=	3;		// pin 18
 
+// commands
 const char CMD_SET_DRIVE_LEFT_FORWARD[]		= "sdlf";
 const char CMD_SET_DRIVE_LEFT_BACKWARD[]	= "sdlb";
 const char CMD_SET_DRIVE_LEFT_STOP[]		= "sdls";
+const char CMD_GET_DRIVE_LEFT_RATE[]		= "gdlr";
 
 const char CMD_SET_DRIVE_RIGHT_FORWARD[]	= "sdrf";
 const char CMD_SET_DRIVE_RIGHT_BACKWARD[]	= "sdrb";
 const char CMD_SET_DRIVE_RIGHT_STOP[]		= "sdrs";
+const char CMD_GET_DRIVE_RIGHT_RATE[]		= "gdrr";
 
 #endif
