7ce841efe835527f0a351a186d575991f733e001
Max <mail@ne0h.de>
2015-04-16 10:58:49 +0200

    enginecontrol: consistent file formats

--- arduino_workspace/enginecontrol/src/constants.hpp
+++ arduino_workspace/enginecontrol/src/constants.hpp
@@ -1,5 +1,6 @@
-#ifndef SRC_CONSTANTS_H_
-#define SRC_CONSTANTS_H_
+#ifndef SRC_CONSTANTS_HPP_
+#define SRC_CONSTANTS_HPP_
+
 #include <stdint.h>
 
 const uint32_t	BAUDRATE		=	115200;
@@ -9,12 +10,12 @@ const uint8_t	CMD_LENGTH		=	4;
 const uint8_t	DRIVE_LEFT_EN	=	35;
 const uint8_t	DRIVE_LEFT_DIR	=	34;
 const uint8_t	DRIVE_LEFT_SPD	=	3;
-const uint8_t	DRIVE_LEFT_MNT	=	5;	// pin 18
+const uint8_t	DRIVE_LEFT_MNT	=	5;		// pin 18
 
 const uint8_t	DRIVE_RIGHT_EN	=	37;
 const uint8_t	DRIVE_RIGHT_DIR =	36;
 const uint8_t	DRIVE_RIGHT_SPD	=	2;
-const uint8_t	DRIVE_RIGHT_MNT	=	4;	// pin 19
+const uint8_t	DRIVE_RIGHT_MNT	=	4;		// pin 19
 
 const char CMD_SET_DRIVE_LEFT_FORWARD[]		= "sdlf";
 const char CMD_SET_DRIVE_LEFT_BACKWARD[]	= "sdlb";
