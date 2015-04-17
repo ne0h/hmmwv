9f95207acee0b146dc727dd76e14805fab411399
Max <mail@ne0h.de>
2015-04-17 15:47:53 +0200

    enginecontrol: state of the art. does not work and we have no idea why

--- arduino_workspace/enginecontrol/src/main.cpp
+++ arduino_workspace/enginecontrol/src/main.cpp
@@ -1,25 +1,27 @@
 #include <Arduino.h>
 
-#include "constants.h"
+#include "constants.hpp"
 #include "monitor.hpp"
 
 char buffer[BUFFER_LENGTH];
 uint8_t buffer_pointer;
 bool cmd_available;
 
-// The motor monitor ISR counts ticks into this variable
-volatile uint8_t leftCurrentTicks = 0;
-volatile uint8_t rightCurrentTicks = 0;
-// Another timed ISR regularly copies the value of currentTicks into this
-// variable to make it available for reading via serial.
-volatile uint8_t leftAggregatedTicks = 0;
-volatile uint8_t rightAggregatedTicks = 0;
+volatile uint32_t drive_left_mnt_start;
+volatile uint32_t drive_left_mnt_cur;
+volatile uint32_t drive_right_mnt_start;
+volatile uint32_t drive_right_mnt_cur;
 
 void uart_prints(char input[], const uint8_t length) {
 	for (uint8_t i = 0; i < length; i++) {
 		Serial.print(input[i]);
 	}
 
+	Serial.print('\n');
+}
+
+void uart_print_success() {
+	Serial.print(0x00);
 	Serial.print('\n');
 }
 
@@ -37,15 +39,26 @@ void cmd() {
 		digitalWrite(DRIVE_LEFT_DIR, LOW);
 		analogWrite(DRIVE_LEFT_SPD, buffer[4]);
 
+		uart_print_success();
+
 	// backward
 	} else if (strncmp(cmd, CMD_SET_DRIVE_LEFT_BACKWARD, CMD_LENGTH) == 0) {
 		digitalWrite(DRIVE_LEFT_EN, HIGH);
 		digitalWrite(DRIVE_LEFT_DIR, HIGH);
 		analogWrite(DRIVE_LEFT_SPD, buffer[4]);
 
+		uart_print_success();
+
 	// stop
 	} else if (strncmp(cmd, CMD_SET_DRIVE_LEFT_STOP, CMD_LENGTH) == 0) {
 		digitalWrite(DRIVE_LEFT_EN, LOW);
+
+		uart_print_success();
+
+	// get rate
+	} else if (strncmp(cmd, CMD_GET_DRIVE_LEFT_RATE, CMD_LENGTH) == 0) {
+		Serial.print(drive_left_mnt_cur);
+		Serial.print('\n');
 
 	/**
 	 * drive engine right side
@@ -57,58 +70,66 @@ void cmd() {
 		digitalWrite(DRIVE_RIGHT_DIR, HIGH);
 		analogWrite(DRIVE_RIGHT_SPD, buffer[4]);
 
+		uart_print_success();
+
 	// backward
 	} else if (strncmp(cmd, CMD_SET_DRIVE_RIGHT_BACKWARD, 4) == 0) {
 		digitalWrite(DRIVE_RIGHT_EN, HIGH);
 		digitalWrite(DRIVE_RIGHT_DIR, LOW);
 		analogWrite(DRIVE_RIGHT_SPD, buffer[4]);
 
+		uart_print_success();
+
 	// stop
 	} else if (strncmp(cmd, CMD_SET_DRIVE_RIGHT_STOP, 4) == 0) {
 		digitalWrite(DRIVE_RIGHT_EN, LOW);
 
-	/**
-	 * wrong cmd
-	 */
-	} else {
-		Serial.print("invalid commands");
-		uart_prints(cmd, CMD_LENGTH);
+		uart_print_success();
+
+	// get rate
+	} else if (strncmp(cmd, CMD_GET_DRIVE_RIGHT_RATE, CMD_LENGTH) == 0) {
+		Serial.print(drive_right_mnt_cur);
+		Serial.print('\n');
 	}
+
 }
 
 /*
  * Interrupt Service Routine for the left drive motor monitor.
- * Careful, this might be called at up to 500 Hz.
  */
-void isr_drive_left_mnt()
-{
-	/*
-	 * Inside the attached function, delay() won't work and the value returned
-	 * by millis() will not increment. Serial data received while in the
-	 * function may be lost. You should declare as volatile any variables that
-	 * you modify within the attached function.
-	 */
-	leftCurrentTicks++;
+void isr_drive_left_mnt() {
+	uint32_t cur = micros();
+	drive_left_mnt_cur   = cur - drive_left_mnt_start;
+	drive_left_mnt_start = cur;
 }
 
 /*
  * Interrupt Service Routine for the right drive motor monitor.
- * Careful, this might be called at up to 500 Hz.
  */
-void isr_drive_right_mnt()
-{
-	rightCurrentTicks++;
+void isr_drive_right_mnt() {
+	uint32_t cur = micros();
+	drive_right_mnt_cur   = cur - drive_right_mnt_start;
+	drive_right_mnt_start = cur;
 }
 
-/*
- * ISR that makes current motor tick counts available for reading.
- */
-void isr_drive_mnt_reset()
-{
-	leftAggregatedTicks = leftCurrentTicks;
-	leftCurrentTicks = 0;
-	rightAggregatedTicks = rightCurrentTicks;
-	rightCurrentTicks = 0;
+void serialEvent() {
+	while (Serial.available()) {
+		const char c = Serial.read();
+		buffer[buffer_pointer] = c;
+
+		if (c == '\n') {
+			cmd();
+			buffer_pointer = 0;
+		} else {
+			buffer_pointer++;
+		}
+
+		// check for buffer overflows
+		if (buffer_pointer >= BUFFER_LENGTH) {
+			buffer_pointer = 0;
+			cmd_available  = true;
+		}
+	}
 }
 
 void setup() {
@@ -133,13 +154,16 @@ void setup() {
 	digitalWrite(DRIVE_RIGHT_DIR, LOW);
 	analogWrite(DRIVE_RIGHT_SPD, 0);
 
-	monitor_write("pin modes set", 13);
+	monitor_write("Pin modes set", 13);
 
 	// setup motor encoder listeners
-	attachInterrupt(DRIVE_LEFT_MNT, &isr_drive_left_mnt, RISING);
-	attachInterrupt(DRIVE_RIGHT_MNT, &isr_drive_right_mnt, RISING);
-	// If we had Timer3 available, we could do
-	//Timer3.attachInterrupt(&isr_drive_mnt_reset, 100000 /*µs = 10 Hz*/);
+	drive_left_mnt_start  = micros();
+	drive_right_mnt_start = micros();
+	drive_left_mnt_cur    = 0;
+	drive_right_mnt_cur   = 0;
+
+	attachInterrupt(DRIVE_LEFT_MNT,  isr_drive_left_mnt,  RISING);
+	attachInterrupt(DRIVE_RIGHT_MNT, isr_drive_right_mnt, RISING);
 }
 
 void loop() {
@@ -149,24 +173,4 @@ void loop() {
 		cmd();
 	}
 
-}
-
-void serialEvent() {
-	while (Serial.available()) {
-		const char c = Serial.read();
-		buffer[buffer_pointer] = c;
-
-		if (c == '\n') {
-			cmd();
-			buffer_pointer = 0;
-		} else {
-			buffer_pointer++;
-		}
-
-		// check for buffer overflows
-		if (buffer_pointer >= BUFFER_LENGTH) {
-			buffer_pointer = 0;
-			cmd_available  = true;
-		}
-	}
 }
--- arduino_workspace/enginecontrol/src/monitor.cpp
+++ arduino_workspace/enginecontrol/src/monitor.cpp
@@ -1,15 +1,15 @@
 #include "monitor.hpp"
 
 void monitor_init(const uint32_t baudrate) {
-	Serial2.begin(baudrate);
+	Serial3.begin(baudrate);
 }
 
 void monitor_write(const char msg[], const uint8_t size) {
-	Serial2.print("DEBUG: ");
+	Serial3.print("DEBUG: ");
 
 	for (uint8_t i = 0; i < size; i++) {
-		Serial2.print(msg[i]);
+		Serial3.print(msg[i]);
 	}
 
-	Serial2.print("\n\r");
+	Serial3.print("\n\r");
 }
