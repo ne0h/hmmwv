300aad7871668fcec6d16682644073e89e4111cc
Marcel <marcel@nightsoul.org>
2015-04-23 18:16:32 +0200

    Switch to hector_mapping and start navigation
    Removed odometry from enginecontrol for that, hector provides the odom TF. Navigation doesn't work yet.

--- ros_workspace/src/hmmwv/src/enginecontrol.cpp
+++ ros_workspace/src/hmmwv/src/enginecontrol.cpp
@@ -8,6 +8,7 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
+#include <math.h>
 #include <iostream>
 #include <sstream>
 
@@ -17,18 +18,26 @@ using namespace std;
 // the motor controllers. (Yeah, recursion!)
 // (C file descriptor)
 int tty;
+std::stringstream ss;
 
 // for odometry
-ros::Time currentTime;
-ros::Time lastTime;
-ros::Publisher odomPub;
-boost::shared_ptr<tf::TransformBroadcaster> odomBroadcaster;
-double x = 0;
-double y = 0;
-double theta = 0;
-double vx = 0;
-double vy = 0;
-double vtheta = 0;
+// ros::Time currentTime;
+// ros::Time lastTime;
+// ros::Publisher odomPub;
+// boost::shared_ptr<tf::TransformBroadcaster> odomBroadcaster;
+// double x = 0;
+// double y = 0;
+// double theta = 0;
+// double vx = 0;
+// double vy = 0;
+// double vtheta = 0;
+// // direction cache (computed in velocityCallback, used for odometry)
+// int leftDirOdo = 1;
+// int rightDirOdo = 1;
+
+const double WHEEL_DIAMETER		= 0.155;
+const double WHEEL_OFFSET		= 0.9;//0.215;
+const double WHEEL_REDUCTION	= 86.0;
 
 // Used to form the Arduino commands
 const char MOTOR_LEFT = 'l';
@@ -52,16 +61,17 @@ bool initTty()
 	tcgetattr(tty, &settings);
 	cfsetospeed(&settings, B115200);
 	cfsetispeed(&settings, B0 /*same as output*/);
-	settings.c_cflag &= ~PARENB;
-	settings.c_cflag &= ~CSTOPB;
-	settings.c_cflag &= ~CSIZE;
+	settings.c_cflag &= ~PARENB;	// no parity
+	settings.c_cflag &= ~CSTOPB;	// one stop bit
+	settings.c_cflag &= ~CSIZE;		// 8 bits per character
 	settings.c_cflag |= CS8;
-	settings.c_cflag &= ~CRTSCTS;
-	settings.c_cflag &= ~HUPCL;
-	settings.c_cflag |= CREAD | CLOCAL;
-	settings.c_iflag &= ~(IXON | IXOFF | IXANY);
-	settings.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-	settings.c_oflag &= ~OPOST;
+	settings.c_cflag &= ~CRTSCTS;	// Disable hardware flow control
+	// settings.c_cflag &= ~HUPCL;		// Send modem disconnect when closing the fd
+	settings.c_cflag |= CREAD;		// Enable receiver
+	// settings.c_cflag |= CLOCAL;		// Ignore modem status lines
+	settings.c_iflag &= ~(IXON | IXOFF | IXANY); // Disable start/stop I/O control
+	settings.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // Disable user terminal features
+	settings.c_oflag &= ~OPOST;		// Disable output postprocessing
 	settings.c_cc[VMIN]  = 0;
 	settings.c_cc[VTIME] = 0;
 	if(tcsetattr(tty, TCSANOW, &settings) != 0) {
@@ -72,9 +82,33 @@ bool initTty()
 	return true;
 }
 
+int readLine(char* const line, const ssize_t LINE_LENGTH)
+{
+	ssize_t bytesRead = 0;
+	bool lineComplete = false;
+	while(!lineComplete && bytesRead < LINE_LENGTH) {
+		// Read bytes into temporary buffer
+		char buffer[LINE_LENGTH];
+		// ROS_INFO("a");
+		ssize_t newBytesRead = read(tty, buffer, LINE_LENGTH);
+		// ROS_INFO("b");
+		if(newBytesRead < 0) {
+			perror("read");
+		}
+		// Copy new bytes to 'line'
+		for(ssize_t i = 0; i < newBytesRead && bytesRead < LINE_LENGTH; i++, bytesRead++) {
+			line[bytesRead] = buffer[i];
+			if(buffer[i] == '\n') {
+				lineComplete = true;
+			}
+		}
+	}
+	return bytesRead;
+}
+
 void setDrive(const char motor, const char direction, const float spd = 0.0f)
 {
-	std::stringstream ss;
+	ss.str("");
 	ss << "sd" << motor << direction;
 	if(direction != MOTOR_STOP) {
 		ss << std::hex << (int8_t)(spd * 255);
@@ -82,28 +116,64 @@ void setDrive(const char motor, const char direction, const float spd = 0.0f)
 	ss << std::endl;
 	const char* output = ss.str().c_str();
 	write(tty, output, ss.str().length());
-	ROS_INFO("%s", output);
+	// ROS_INFO("%s", output);
+	// check response
+	char line[2];
+	const int lineLength = readLine(line, 2);
+	if(atoi(line) != 0) {
+		ROS_INFO("Command failed: %s", output);
+	}
 }
 
 void setRotation(const char motor, const char direction, const float spd = 0.0f)
 {
-	std::stringstream ss;
+	ss.str("");
 	ss << "sr" << motor << direction;
 	if(direction != MOTOR_STOP) {
 		ss << std::hex << (int8_t)(spd * 255);
 	}
 	ss << std::endl;
 	const char* output = ss.str().c_str();
+	// write(tty, output, ss.str().length());
+	// check response
+	// char line[2];
+	// const int lineLength = readLine(line, 2);
+	// if(atoi(line) != 0) {
+	// 	ROS_INFO("Command failed: %s", output);
+	// }
+}
+
+/*
+ * Returns the speed in m/s the given motor is moving the robot at.
+ */
+float getSpeed(const char motor)
+{
+	// Write command
+	ss.str("");
+	// "get drive left/right rate"
+	ss << "gd" << motor << 'r' << std::endl;
+	const char* output = ss.str().c_str();
 	write(tty, output, ss.str().length());
-	//ROS_INFO("%s", output);
+	// ROS_INFO("%s", ss.str().c_str());
+	
+	// Read response
+	// Expected: max. 10 characters (2,147,483,647) + \n
+	const int LINE_LENGTH = 11;
+	char line[LINE_LENGTH] = {0};
+	const int lineLength = readLine(line, LINE_LENGTH);
+
+	// We receive the interval in µs bewtween two motor monitor ticks
+	const int interval = atoi(line);
+	// Compute motor revolution freq (Hz) from motor tick interval (µs)
+	// Filter out division by zero and "stop" state
+	const double freq = (interval == 0 || interval == 1000000) ? 0 : 1000000 / interval;
+	// if(motor == MOTOR_RIGHT)
+		ROS_INFO("interval %c: %i freq: %f", motor, interval, freq);
+	// Return the resulting robot speed for this motor (m/s)
+	return freq * WHEEL_DIAMETER * M_PI / (3.0 * WHEEL_REDUCTION);
 }
 
 void velocityCallback(const geometry_msgs::Twist& msg) {
-	// Store odometry input values
-	vx = msg.linear.x * 1.164 /* m/s robot speed*/;
-	vy = msg.linear.y * 1.164 /*m/s*/;
-	vtheta = msg.angular.z * 1.732 /*rad/s*/ * 1.1;
-
 	// Tank-like steering (rotate around vehicle center)
 	// Set linear back/forward speed
 	double leftSpd = msg.linear.x;
@@ -112,8 +182,12 @@ void velocityCallback(const geometry_msgs::Twist& msg) {
 	leftSpd -= msg.angular.z;
 	rightSpd += msg.angular.z;
 	// Determine rotation directions
+	// (Yes, having two kinds of "direction" variables sucks badly. Another take
+	// at object orientation should be done to fix this.)
 	char leftDir = leftSpd > 0 ? MOTOR_FORWARD : MOTOR_BACKWARD;
 	char rightDir = rightSpd > 0 ? MOTOR_FORWARD : MOTOR_BACKWARD;
+	// leftDirOdo = leftSpd > 0 ? 1 : -1;
+	// rightDirOdo = rightSpd > 0 ? 1 : -1;
 	// Map [-1, 1] -> [0, 1] as we've extracted the directional component
 	leftSpd = leftSpd < 0 ? leftSpd * -1.0 : leftSpd;
 	rightSpd = rightSpd < 0 ? rightSpd * -1.0 : rightSpd;
@@ -145,64 +219,72 @@ void velocityCallback(const geometry_msgs::Twist& msg) {
 	if(rightRotSpd < STOP_THRESHOLD) {
 		rightRotDir = MOTOR_STOP;
 	}
-	//setRotation(MOTOR_LEFT, leftRotDir, leftRotSpd);
-	//setRotation(MOTOR_RIGHT, rightRotDir, rightRotSpd);
+	setRotation(MOTOR_LEFT, leftRotDir, leftRotSpd);
+	setRotation(MOTOR_RIGHT, rightRotDir, rightRotSpd);
 }
 
-void publishOdometry(const ros::TimerEvent&) {
-	// Source: http://wiki.ros.org/navigation/Tutorials/RobotSetup/Odom
-	// Compute input values
-	double dt = (currentTime - lastTime).toSec();
-	lastTime = currentTime;
-	currentTime = ros::Time::now();
-	double dx = (vx * cos(theta) - vy * sin(theta)) * dt;
-	double dy = (vx * sin(theta) - vy * cos(theta)) * dt;
-	double dtheta = vtheta * dt;
-	x += dx;
-	y += dy;
-	theta += dtheta;
+// void publishOdometry(const ros::TimerEvent&) {
+// 	// Source: http://wiki.ros.org/navigation/Tutorials/RobotSetup/Odom
+// 	// Store odometry input values
+// 	// double leftSpd = getSpeed(MOTOR_LEFT) * leftDirOdo;
+// 	// double rightSpd = getSpeed(MOTOR_RIGHT) * rightDirOdo;
+// 	// vx = (leftSpd + rightSpd) / 2.0;
+// 	// vtheta = (rightSpd - leftSpd) / WHEEL_OFFSET;
 
-	// Transform frame
-	//since all odometry is 6DOF we'll need a quaternion created from yaw
-	geometry_msgs::Quaternion odomQuat = tf::createQuaternionMsgFromYaw(theta);
+// 	// Compute input values
+// 	// double dt = (currentTime - lastTime).toSec();
+// 	// lastTime = currentTime;
+// 	// currentTime = ros::Time::now();
+// 	// double dx = (vx * cos(theta) - vy * sin(theta)) * dt;
+// 	// double dy = (vx * sin(theta) - vy * cos(theta)) * dt;
+// 	// vtheta = (dx - dy) / WHEEL_OFFSET;
+// 	// double dtheta = vtheta * dt;
+// 	// x += dx;
+// 	// y += dy;
+// 	// theta += dtheta;
+// 	// ROS_INFO("vl: %f vr: %f vx: %f vtheta: %f", leftSpd, rightSpd, vx, vtheta);
 
-	geometry_msgs::TransformStamped odomTrans;
-	odomTrans.header.stamp = currentTime;
-	odomTrans.header.frame_id = "odom";
-	odomTrans.child_frame_id = "base_link";
+// 	// Transform frame
+// 	//since all odometry is 6DOF we'll need a quaternion created from yaw
+// 	geometry_msgs::Quaternion odomQuat = tf::createQuaternionMsgFromYaw(theta);
 
-	odomTrans.transform.translation.x = x;
-	odomTrans.transform.translation.y = y;
-	odomTrans.transform.translation.z = 0.0;
-	odomTrans.transform.rotation = odomQuat;
+// 	geometry_msgs::TransformStamped odomTrans;
+// 	odomTrans.header.stamp = currentTime;
+// 	odomTrans.header.frame_id = "odom";
+// 	odomTrans.child_frame_id = "base_link";
 
-	odomBroadcaster->sendTransform(odomTrans);
+// 	odomTrans.transform.translation.x = x;
+// 	odomTrans.transform.translation.y = y;
+// 	odomTrans.transform.translation.z = 0.0;
+// 	odomTrans.transform.rotation = odomQuat;
 
-	// Odometry message
-	nav_msgs::Odometry odom;
-	odom.header.stamp = currentTime;
-	odom.header.frame_id = "odom";
+// 	odomBroadcaster->sendTransform(odomTrans);
 
-	//set the position
-	odom.pose.pose.position.x = x;
-	odom.pose.pose.position.y = y;
-	odom.pose.pose.position.z = 0.0;
-	odom.pose.pose.orientation = odomQuat;
+// 	// Odometry message
+// 	nav_msgs::Odometry odom;
+// 	odom.header.stamp = currentTime;
+// 	odom.header.frame_id = "odom";
 
-	//set the velocity
-	odom.child_frame_id = "base_link";
-	odom.twist.twist.linear.x = vx;
-	odom.twist.twist.linear.y = vy;
-	odom.twist.twist.angular.z = vtheta;
+// 	//set the position
+// 	odom.pose.pose.position.x = x;
+// 	odom.pose.pose.position.y = y;
+// 	odom.pose.pose.position.z = 0.0;
+// 	odom.pose.pose.orientation = odomQuat;
 
-	odomPub.publish(odom);
-}
+// 	//set the velocity
+// 	odom.child_frame_id = "base_link";
+// 	odom.twist.twist.linear.x = vx;
+// 	odom.twist.twist.linear.y = vy;
+// 	odom.twist.twist.angular.z = vtheta;
+
+// 	odomPub.publish(odom);
+// }
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "enginecontrol");
 	ros::NodeHandle n;
-	currentTime = ros::Time::now();
-	lastTime = ros::Time::now();
+	// currentTime = ros::Time::now();
+	// lastTime = ros::Time::now();
 	if(!initTty()) {
 		return 1;
 	}
@@ -215,9 +297,9 @@ int main(int argc, char **argv) {
 
 	// This is correct - we're borrowing the turtle's topics
 	ros::Subscriber sub = n.subscribe("turtle1/cmd_vel", 1, velocityCallback);
-	odomPub = n.advertise<nav_msgs::Odometry>("odom", 50);
-	odomBroadcaster = boost::make_shared<tf::TransformBroadcaster>();
-	ros::Timer odoTimer = n.createTimer(ros::Duration(1.0/2.0/*2 Hz*/), publishOdometry);
+	//odomPub = n.advertise<nav_msgs::Odometry>("odom", 50);
+	//odomBroadcaster = boost::make_shared<tf::TransformBroadcaster>();
+	//ros::Timer odoTimer = n.createTimer(ros::Duration(1.0/2.0/*2 Hz*/), publishOdometry);
 	ROS_INFO("enginecontrol up and running.");
 	ros::spin();
 	close(tty);
