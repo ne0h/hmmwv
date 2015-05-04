75b7df525535c572736f8d52a4e7309531632270
Max <mail@ne0h.de>
2015-05-04 19:26:09 +0200

    constants conflict fix

--- ros_workspace/src/hmmwv/src/constants.hpp
+++ ros_workspace/src/hmmwv/src/constants.hpp
@@ -5,8 +5,8 @@ const float MAX_DRV_SPEED = 1.642; /* m/s */
 const float MAX_TURN_SPEED = 1.732; /* rad/s */
 const float MAX_ROT_SPEED = 1; /* rad/s */
 
-const double WHEEL_DIAMETER		= 0.155;
-const double WHEEL_DISTANCE		= 0.52;
+const double WHEEL_DIAMETER	= 0.155;
+const double WHEEL_DISTANCE	= 0.52;
 const double WHEEL_REDUCTION	= 86.0;
 
 #endif
