aabc8fe99cd65cf1d804b18222f43fffde6eb691
Max <mail@ne0h.de>
2015-12-02 17:27:39 +0100

    [stairs] some canny updates

--- ros_workspace/src/hmmwv/nodes/detectstairs.py
+++ ros_workspace/src/hmmwv/nodes/detectstairs.py
@@ -1,30 +1,39 @@
+#!/usr/bin/env python
+
+import rospy, message_filters
+from sensor_msgs.msg import Image
+
 import cv2, math, numpy
 from operator import itemgetter
+from cv_bridge import CvBridge, CvBridgeError
 
-def findStairway(srcImage, depthImage):
+def findStairway(rgbImage, depthImage):
 
 	# transform source image to grayscale and afterwards to binary
-	grayImage   = cv2.cvtColor(srcImage, cv2.COLOR_BGR2GRAY)
-	binaryImage = cv2.adaptiveThreshold(~grayImage, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, -2)
+	grayImage   = cv2.cvtColor(rgbImage, cv2.COLOR_BGR2GRAY)
 
-	lines = binaryImage.shape[1] / 30
+	lines = grayImage.shape[1] / 30
 	structure = cv2.getStructuringElement(cv2.MORPH_RECT, (lines, 1))
 
-	binaryImage = cv2.erode(binaryImage, structure, (-1, -1))
-	binaryImage = cv2.dilate(binaryImage, structure, (-1, -1))
+	grayImage = cv2.erode(grayImage, structure, (-1, -1))
+	grayImage = cv2.dilate(grayImage, structure, (-1, -1))
 
-	edges = cv2.Canny(binaryImage, 50, 100, apertureSize=3)
-	lines = cv2.HoughLinesP(edges, 1, math.pi/180, 150, 0, 0)
+	edges = cv2.Canny(grayImage, 100, 200)
+	cv2.imwrite("canny.jpg", edges)
+	lines = cv2.HoughLinesP(edges, 1, math.pi/180, 80, 30, 10)
 
-	height, width = binaryImage.shape
+	# dimensions of the input images
+	height, width = grayImage.shape
+
 	coords = []
 	for l in lines[0]:
+		# 				left point   right point   average height
 		coords.append(((l[0], l[1]), (l[2], l[3]), (l[1]+l[3])/2))
 
 	#
 	# sort lines from bottom to top
 	#
-	coords.sort(key=lambda tup: (tup[0][1]+tup[1][1])/2, reverse=True)
+	coords.sort(key=lambda tUp: (tUp[0][1]+tUp[1][1])/2, reverse=True)
 
 	#
 	# filter lines width nearly identical height values and take averge values
@@ -33,24 +42,22 @@ def findStairway(srcImage, depthImage):
 	skipNext = False
 	threshold = 20
 	for i in range(0, len(coords)):
-		p, q, m = coords[i]
+		p, q, avgHgt = coords[i]
 		if skipNext:
 			skipNext = False
 		else:
 			# filter lowest 20 pixels
-			if height - m > 20:
-				if i < len(coords) - 1 and (m - coords[i+1][2]) < threshold:
+			if height - avgHgt > 20:
+				if i < len(coords) - 1 and (avgHgt - coords[i+1][2]) < threshold:
 					pNew = ((p[0] + coords[i+1][0][0]) / 2, (p[1] + coords[i+1][0][1]) / 2)
 					qNew = ((q[0] + coords[i+1][1][0]) / 2, (q[1] + coords[i+1][1][1]) / 2)
-					filteredCoords.append((pNew, qNew, m))
+					filteredCoords.append((pNew, qNew, avgHgt))
 					skipNext = True
 				else:
-					filteredCoords.append((p, q, m))
-
-		i += 1
+					filteredCoords.append((p, q, avgHgt))
 
 	#
-	# filter lines with same depth value
+	# Remove lines with same depth value.
 	#
 	secTimeFiltered = []
 	skipNext = False
@@ -61,40 +68,48 @@ def findStairway(srcImage, depthImage):
 		else:
 			if i < len(filteredCoords) - 1:
 				mNext = filteredCoords[i+1][2]
+
+				# add this line and check if the next line has the same depth value.
+				# If so skip the next one
 				secTimeFiltered.append((p, q))
-				if depthImage[m][width/2][0] == depthImage[mNext][width/2][0]:
+
+				if depthImage[m][width/2] == depthImage[mNext][width/2]:
 					skipNext = True
-		i += 1
 
 	#
-	# try to enlarge lines in both directions. take depth values
+	# Try to enlarge lines in both directions. Take depth values
 	#
-	step = 5
-	threshold = 3
+	step = 1
+	threshold = 0.05
 	for i in range(0, len(secTimeFiltered) - 1):
 		p, q = secTimeFiltered[i]
 		
 		# enlarge p to the left
-		d  = int(depthImage[p[1]][p[0]][0])
+		d  = float(depthImage[p[1]][p[0]])
 		dp = p[0]
 		while (dp > 0):
-			if math.fabs(int(depthImage[p[1]][dp][0]) - d) < threshold:
+			if math.fabs(float(depthImage[p[1]][dp]) - d) < threshold:
 				dp -= 10
 			else:
 				break
 
 		# enlarge q to the right
-		d  = int(depthImage[q[1]][q[0]][0])
+		d  = float(depthImage[q[1]][q[0]])
 		dq = q[0]
 		while (dq < width):
-			if math.fabs(int(depthImage[q[1]][dq][0]) - d) < threshold:
+			if math.fabs(float(depthImage[q[1]][dq]) - d) < threshold:
 				dq += 10
 			else:
 				break
 		secTimeFiltered[i] = ((dp, p[1]), (dq, q[1]))
 
+	for i in range(0, len(secTimeFiltered)):
+		p, q = secTimeFiltered[i]
+		cv2.line(rgbImage, (p[0], p[1]), (q[0], q[1]), (0, 0, 255), 2)
+	cv2.imwrite("rgb.jpg", rgbImage)
+
 	#
-	# filter again lines width nearly identical height values and take averge value
+	# Filter again lines width nearly identical height values and take averge value.
 	#
 	trdTimeFiltered = []
 	skipNext = False
@@ -114,60 +129,77 @@ def findStairway(srcImage, depthImage):
 	# draw a line for each line
 	for i in range(0, len(trdTimeFiltered)):
 		p, q = trdTimeFiltered[i]
-		cv2.line(srcImage, (p[0], p[1]), (q[0], q[1]), (0, 0, 255), 2)
+		cv2.line(rgbImage, (p[0], p[1]), (q[0], q[1]), (0, 0, 255), 2)
 
-	stairsFound = []
-	threshold = 3
-	# Compare depth values of every second line with the depth value of the next line to the bottom and the next line to
-	# the top to find stairs.
-	# If the depth value does not fit (e.g., due to artefacts on the depth image) check points next to this one.
+	# Compare depth values of every second line with the depth value of the next line to
+	# the bottom and the next line to the top to find stairs.
+	# If the depth value does not fit (e.g., due to artefacts on the depth image)
+	# check points next to this one.
+	stairFronts = []
+	threshold = 0.1
 	for i in range(0, len(trdTimeFiltered)):
 			p, q = trdTimeFiltered[i]
-			depth = int(depthImage[p[1]][p[0]][0])
+			depth = float(depthImage[p[1]][p[0]])
 
 			# compare values, check if the indexes exist before
 			# start with value next to the top...
 			if i < len(trdTimeFiltered) - 1:
 				pNext, qNext = trdTimeFiltered[i+1]
-				depthNext = int(depthImage[pNext[1]][qNext[0]][0])
+				try:
+					depthNext = float(depthImage[pNext[1]][qNext[0]])
+				except:
+					print("at shit")
+					print(pNext)
+					print(qNext)
 
 				if math.fabs(depth - depthNext) < threshold:
-					stairsFound.append((pNext, q))
+					stairFronts.append((pNext, q))
 				# if nothing found check value 
 				else:
 					newCoords = (pNext[0], pNext[1] - 30)
-					depthNew = int(depthImage[newCoords[1]][newCoords[0]][0])
+					depthNew = float(depthImage[newCoords[1]][newCoords[0]])
 					if math.fabs(depth - depthNew) < 15:
-						stairsFound.append((pNext, q))
+						stairFronts.append((pNext, q))
 			# ...and go on with prev value
 			if i > 1:
 				pPrev, qPrev = trdTimeFiltered[i-1]
-				depthPrev = depthNext = int(depthImage[pPrev[1]][qPrev[0]][0])
+				depthPrev = depthNext = float(depthImage[pPrev[1]][qPrev[0]])
 
 				if math.fabs(depth - depthPrev) < threshold:
-					stairsFound.append((p, qPrev))
+					stairFronts.append((p, qPrev))
 				# if nothing found check value 
 				else:
 					newCoords = (pPrev[0], pPrev[1] + 30)
-					depthNew = int(depthImage[newCoords[1]][newCoords[0]][0])
+					depthNew = float(depthImage[newCoords[1]][newCoords[0]])
 					if math.fabs(depth - depthNew) < 15:
-						stairsFound.append((p, qPrev))
+						stairFronts.append((p, qPrev))
 
-	return (len(stairsFound) > 1)
+	# Removes
 
-"""
-srcImage = cv2.imread("stairs.png")
-if not srcImage.data: print("Failed to load image.")
+	for p, q in stairFronts:
+		cv2.rectangle(rgbImage, p, q, (0, 255, 0))
+	#cv2.imwrite("rgb.jpg", rgbImage)
 
-depthImage = cv2.imread("depth.png")
-if not depthImage.data: print("Failed to load depth image.")
+	return (len(stairFronts) > 1)
 
-if findStairway(srcImage, depthImage):
-	print("Stairway")
-else:
-	print("No stairway")
-"""
+# Needs to convert ros images to opencv images (numpy array)
+def callback(rgbImage, depthImage):
+
+	if findStairway(CvBridge().imgmsg_to_cv2(rgbImage, "bgr8"),
+			CvBridge().imgmsg_to_cv2(depthImage)):
+		rospy.loginfo("Stairway!")
+	else:
+		rospy.loginfo("No stairway.")
+	import sys
+	sys.exit()
 
 if __name__ == "__main__":
 	rospy.init_node("stairsdetection")
-	rospy.Subscriber("/camera/image/compressed", CompressedImage, ycallback,  queue_size = 1)
+	
+	rgbImgSub   = message_filters.Subscriber("/camera/rgb/image_color", Image)
+	depthImgSub = message_filters.Subscriber("/camera/depth/image", Image)
+	
+	ts = message_filters.ApproximateTimeSynchronizer([rgbImgSub, depthImgSub], 10, 1)
+	ts.registerCallback(callback)
+
+	rospy.spin()
