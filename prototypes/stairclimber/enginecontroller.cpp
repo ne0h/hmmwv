27284eff89ba3f56d8e4b511c7b97d0b01486468
Max <mail@ne0h.de>
2016-06-14 18:05:25 +0000

    [prototype] basic engine control works

--- prototypes/stairclimber/enginecontroller.cpp
+++ prototypes/stairclimber/enginecontroller.cpp
@@ -9,69 +9,59 @@
 #include <cmath>
 
 #include <joystick.hpp>
-#include "i2c.h"
+#include "gpio.hpp"
 
 using namespace std;
 
-const double SENSITIVITY	= 16384;
-const double PROCESS_NOISE	= 0.1;
-const double SENSOR_NOISE	= 64.0;
-const double INITIAL_Q		= 100.0;
-const int AXIS_MAX			= 32767;
+using namespace std;
 
-typedef struct {
-	double q, r, x, p, k;
-} kalman;
-
+const int AXIS_MAX = 32767;
+const double AXIS_THRS = 0.01;
+const double PWM_DUTYMAX = 0.9;
 Joystick joystick;
-kalman kx, ky, kz;
-
-kalman kalman_init(double initial_value) {
-	kalman result;
-
-	result.q = PROCESS_NOISE;
-	result.r = SENSOR_NOISE;
-	result.p = INITIAL_Q;
-	result.x = initial_value;
-
-	return result;
-}
-
-void kalman_update(kalman *k, double value) {
-	k->p = k->p + k->q;
-	k->k = k->p / (k->p + k->r);
-	k->x = k->x + k->k * (value - k->x);
-	k->p = (1 - k->k) * k->p;
-}
+GPIO gpio;
 
 int main() {
-	
-	const int addr = 0x68;
-	const int fd = i2c_init("/dev/i2c-1");
-	
-	uint8_t buf[6] = {0};
-	i2c_write(fd, addr, buf, 1);
-	
-	kx = kalman_init(0);
-	ky = kalman_init(0);
-	kz = kalman_init(0);
-	
-	while (1) {
-		i2c_read(fd, addr, 0x3b, 6, buf);
-		const double ax = (uint16_t(((int16_t)buf[0]) << 8) | buf[1]) / SENSITIVITY;
-		const double ay = (uint16_t(((int16_t)buf[2]) << 8) | buf[3]) / SENSITIVITY;
-		const double az = (uint16_t(((int16_t)buf[4]) << 8) | buf[5]) / SENSITIVITY;
-		
-		kalman_update(&kx, ax);
-		kalman_update(&ky, ay);
-		kalman_update(&kz, az);
 
-		double roll  = atan2(-(ky.x), kz.x) * 180.0/M_PI;
-		double pitch = atan2(kx.x, sqrt(ky.x*ky.x + kz.x*kz.x)) * 180.0/M_PI;
+	if (!joystick.init()) {
+		cout << "Could not find a joystick!\n";
+		return 1;
+	}
+
+	const string name = joystick.getName();
+	cout << "Used controller: " << name.c_str() << endl;
+
+	gpio.setPin(GPIO::P8_08, false);
+	gpio.setPwm(GPIO::P8_13, 0.f);
+
+	bool run = true;
+	while(run) {
+		JoystickEvent event = joystick.getEvent();
+		vector<short> axis = event.getAxis();
+		vector<bool> buttons = event.getButtons();
+
+		if (buttons.at(0)) {
+			run = false;
+		}
+
+		double leftController  = (-1.0) * axis.at(1) / AXIS_MAX;
+		if (leftController >  PWM_DUTYMAX) leftController =  PWM_DUTYMAX;
+		if (leftController < -1.0 * PWM_DUTYMAX) leftController = -1.0 * PWM_DUTYMAX;
+		double rightController = (-1.0) * axis.at(2) / AXIS_MAX;
+		if (rightController >  PWM_DUTYMAX) rightController =  PWM_DUTYMAX;
+		if (rightController < -1.0 * PWM_DUTYMAX) rightController = -1.0 * PWM_DUTYMAX;
 		
-		cout << roll << "\t\t" << pitch << endl;
-		
-		this_thread::sleep_for(std::chrono::milliseconds(10));
+
+		if (abs(leftController - AXIS_THRS) && leftController > 0.0) {
+			gpio.setPin(GPIO::P8_08, true);
+			gpio.setPwm(GPIO::P8_13, abs(leftController));
+		} else if (abs(leftController - AXIS_THRS) && leftController < 0.0) {
+			gpio.setPin(GPIO::P8_08, false);
+			gpio.setPwm(GPIO::P8_13, abs(leftController));
+		} else {
+			gpio.setPin(GPIO::P8_08, false);
+			gpio.setPwm(GPIO::P8_13, 0.0);
+		}
 	}
 
 	return 0;
