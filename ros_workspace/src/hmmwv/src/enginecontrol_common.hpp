9a37cf2481f6eb567d35d5c042cd8cc6f902f92e
Max <mail@ne0h.de>
2019-06-12 11:53:57 +0200

    Test (un-)marshalling of commands.

--- ros_workspace/src/hmmwv/src/enginecontrol_common.hpp
+++ ros_workspace/src/hmmwv/src/enginecontrol_common.hpp
@@ -1,9 +1,25 @@
 #pragma once
 
-class Engine {
-public:
-    uint8_t ready, status, speed, direction, monitor;
-    const uint8_t x;
-
-    Engine(const uint8_t x) : x(x) {}
+enum Direction {
+    CW,
+    CCW,
 };
+
+struct cmd {
+    uint8_t cmd_id:4,
+            engine_id:3,
+            direction:1;
+    uint8_t data;
+};
+
+void marshal(const struct cmd *cmd, uint8_t *buf) {
+    buf[0] = (cmd->cmd_id<<4) + (cmd->engine_id<<1) + cmd->direction;
+    buf[1] = cmd->data;
+}
+
+void unmarshal(struct cmd *cmd, const uint8_t *buf) {
+    cmd->cmd_id = (buf[0]>>4) & 0x0f;
+    cmd->engine_id = (buf[0]>>1) & 0x07;
+    cmd->direction = buf[0] & 0x01;
+    cmd->data = buf[1];
+}
--- ros_workspace/src/hmmwv/src/enginecontrol_common_test.cpp
+++ ros_workspace/src/hmmwv/src/enginecontrol_common_test.cpp
@@ -1,16 +1,58 @@
 #include <cppunit/extensions/HelperMacros.h>
 
-class EngineControlCommonTest : public CppUnit::TestFixture {
+#include <cstdio>
 
+#include "enginecontrol_common.hpp"
+
+struct test {
+	struct cmd in;
+	uint8_t out[2];
+};
+
+class EngineControlCommonTest : public CppUnit::TestFixture {
 	CPPUNIT_TEST_SUITE(EngineControlCommonTest);
-	CPPUNIT_TEST(testMarshal);
+	CPPUNIT_TEST(testMarshalAndUnmarshal);
 	CPPUNIT_TEST_SUITE_END();
 
-public:
-	void testMarshal() {
+private:
+	bool equals(const uint8_t *buf1, const uint8_t *buf2, const size_t length) {
+		for (size_t i = 0; i < length; i++) {
+			if (buf1[i] != buf2[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-		
-		CPPUNIT_ASSERT(0 == 0);
+	bool equals(const cmd *cmd1, const cmd *cmd2) {
+		return (cmd1->cmd_id == cmd2->cmd_id && cmd1->engine_id == cmd2->engine_id
+			&& cmd1->direction == cmd2->direction && cmd1->data == cmd2->data);
+	}
+
+public:
+	void testMarshalAndUnmarshal() {
+		std::vector<struct test> tests {
+			{{0, 0, 0, 0}, 		{0x0, 0x0}},		// stop left engine
+			{{0, 1, 0, 0}, 		{0x2, 0x0}},		// stop right engine
+			{{1, 0, 0, 127}, 	{0x10, 0x7f}},		// rotate left engine CW
+			{{1, 0, 1, 10}, 	{0x11, 0xa}},		// rotate left engine CCW
+			{{1, 1, 0, 255}, 	{0x12, 0xff}},		// rotate right engine CW
+			{{1, 1, 1, 0},	 	{0x13, 0x0}},		// rotate right engine CCW
+		};
+
+		for (auto it = tests.begin(); it != tests.end(); it++) {
+			auto test = (*it);
+			
+			// marshal
+			uint8_t buf[2];
+			marshal(&test.in, buf);
+			CPPUNIT_ASSERT_EQUAL(true, equals(test.out, buf, 2));
+
+			// unmarshal
+			struct cmd newCmd;
+			unmarshal(&newCmd, buf);
+			CPPUNIT_ASSERT_EQUAL(true, equals(&test.in, &newCmd));
+		}
 	}
 
 };
