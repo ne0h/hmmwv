01d20c9fbc95accb2d5f5189d4c335b643ed37cc
Max <mail@ne0h.de>
2019-06-20 14:39:19 +0200

    Remove obsolet scripts.

--- setup_enginecontrol.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git clone https://github.com/ne0h/arduino-core.git
-mv arduino-core/arduino_core arduino_workspace/
-rm -rf arduino-core
--- turtlesim.sh
+++
@@ -1,1 +0,0 @@
-ROS_MASTER_URI=http://i60beagle3:11311 rosrun turtlesim turtlesim_node
